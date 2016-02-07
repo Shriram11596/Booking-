f10761287ea8fefe37b665f73252feb222f3e35d
Shriram11596 <shrirambalaji1996@gmail.com>
2016-02-07 20:45:33 +0530

    pushing changes

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,9 @@ gulp.task('add', function(){
   return gulp.src('./booking/*').pipe(git.add({args: '--all'}));
 });
 
- //Run git commit without checking for a message using raw arguments 
-gulp.task('commit', function(){
-  return gulp.src('./booking/*')
-    .pipe(git.commit(undefined, {
-      args: '-m "initial commit"',
-      disableMessageRequirement: true
-    }));
-});
+    gulp.task('commit',function(){
+  return gulp.src('./booking/*').pipe(git.commit('pushing changes'));
+    });
 
     gulp.task('push', function(){
   git.push('origin', 'master', function (err) {
