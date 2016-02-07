cd51c8a90e391d5b6fd7c397c16eecf5a4b060b9
Shriram11596 <shrirambalaji1996@gmail.com>
2016-02-07 21:14:09 +0530

    commit504

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,10 @@ gulp.task('add', function(){
   return gulp.src('./booking/*').pipe(git.add({args: '--all'}));
 });
 
+var x = Math.floor((Math.random() * 1000) + 1);
+
     gulp.task('commit',['add'],function(){
-  return gulp.src('./booking/*').pipe(git.commit('commit'));
+  return gulp.src('./booking/*').pipe(git.commit('commit'+x));
     });
 
     gulp.task('default',['commit'], function(){
