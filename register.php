59f6d03edc80bbaa33fbada7a3ef8b3a8b9ca680
Shriram11596 <shrirambalaji1996@gmail.com>
2016-02-08 21:16:13 +0530

    commit308

--- register.php
+++ register.php
@@ -12,7 +12,6 @@ $email = $_POST['email'];
 $contact = $_POST['contact'];
 $time = date("Y-m-d H:i:s"); 
 $message = "The cargo of weight 10 kg is booked from chennai to hyderabad by $name . Your number is $contact";
-
 $record = $db->prepare("INSERT INTO REGISTER (NAME,EMAIL,CONTACT) SELECT * FROM (SELECT '$name', '$email', '$contact') AS tmp WHERE NOT EXISTS (SELECT * FROM REGISTER WHERE EMAIL = '$email' OR CONTACT = '$contact') LIMIT 1; ");
 $execute = $record->execute();
 if($record -> rowCount()>0)
