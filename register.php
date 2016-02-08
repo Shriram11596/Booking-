be1c71498195bd97c107915aa502f1201c9a7afe
Shriram11596 <shrirambalaji1996@gmail.com>
2016-02-08 18:08:32 +0530

    commit783

---
+++ register.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+require_once 'connection.php';
+try {
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$contact = $_POST['contact'];
+$time = date("Y-m-d H:i:s"); 
+$message = "The cargo of weight 10 kg is booked from chennai to hyderabad by $name . Your number is $contact";
+
+$record = $db->prepare("INSERT INTO REGISTER (NAME,EMAIL,CONTACT) SELECT * FROM (SELECT '$name', '$email', '$contact') AS tmp WHERE NOT EXISTS (SELECT * FROM REGISTER WHERE EMAIL = '$email' OR CONTACT = '$contact') LIMIT 1; ");
+$execute = $record->execute();
+if($record -> rowCount()>0)
+{
+echo json_encode(array(
+	'success' => 'true',
+	'booked' => 'true',
+	'time' => $time,
+	'owner' => $name,
+	'contact' => $contact,
+	'message' => $message,
+
+	));
+}
+
+else
+{
+	echo json_encode(array(
+	'success' => 'false',
+	'failure' => 'true',
+	'cause' => 'Person already exists',
+
+	));
+}
+
+	
+} catch (Exception $e) {
+	
+}
+
+
+?>
