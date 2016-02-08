21a612ad8032e6561144033374684fbb1627e7b3
Shriram11596 <shrirambalaji1996@gmail.com>
2016-02-08 21:21:37 +0530

    commit453

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 session_start();
 require_once 'connection.php';
 try {
-	
+
 define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST')); //ENSURES THE DB CONNECTION//
 define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
 define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
@@ -24,6 +24,7 @@ $time = date("Y-m-d H:i:s");
 $message = "The cargo of weight 10 kg is booked from chennai to hyderabad by $name . Your number is $contact";
 $record = $db->prepare("INSERT INTO REGISTER (NAME,EMAIL,CONTACT) SELECT * FROM (SELECT '$name', '$email', '$contact') AS tmp WHERE NOT EXISTS (SELECT * FROM REGISTER WHERE EMAIL = '$email' OR CONTACT = '$contact') LIMIT 1; ");
 $execute = $record->execute();
+echo "string";
 if($record -> rowCount()>0)
 {
 echo json_encode(array(
